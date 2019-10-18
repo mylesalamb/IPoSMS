3f2618f4cdb0a43af6fd4d25ee0bfb60ef99a321
myles lamb <mylesalamb@gmail.com>
2019-10-19 00:56:03 +0100

    stuff

--- flask_app.py
+++ flask_app.py
@@ -16,6 +16,7 @@ def sms_reply():
 
     resp = MessagingResponse()
 
+    arg = request.values.get("Body",None)
     #there was no message body
     if not arg:
     	resp.message("The Robots are coming! Head for the hills!")
