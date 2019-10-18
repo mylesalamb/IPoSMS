8e8263b7bf911138e49d5f90c65c20dc08156c8a
myles lamb <mylesalamb@gmail.com>
2019-10-18 22:23:20 +0100

    added json formatting to sending message

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
 from twilio.rest import Client
 from base64 import b64encode
+import json
 ACCOUNT_SID = "AC1ff67468e34c89848511de573a0e5056"
 AUTH = "bfa2d58a73b19954934dbd5038ff7230"
 
 
 
-def send_text(encoded_string, recip='+447758928837'):
+def send_text(encoded_string, filename = "test" ,recip='+447758928837'):
 
 
 	client = Client(ACCOUNT_SID,AUTH)
 	message = client.messages.create(
-	                              body="{ name=  }".format(encoded_string),
+	                              body=  json.dumps({"name":filename,"data":encoded_string}),
 	                              from_='+12162798288',
 	                              to='+447758928837'
 	                          )
@@ -24,5 +25,6 @@ def get_64image(filename):
 
 
 if __name__ == "__main__":
-	response = send_text(get_64image("test.png"))
+	test_file = "test.png"
+	response = send_text(get_64image(test_file),filename=test_file)
 	print(response)
