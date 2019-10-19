815e2faba64585503e69e31239afc3c9a2624283
Nathan Kirkpatrick <nksocial99@gmail.com>
2019-10-19 14:39:12 +0100

    urlparse update

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
+from urllib import urlparse
 
 import requests
 
@@ -16,11 +17,20 @@ def sms_reply():
     resp = MessagingResponse()
     arg = request.values.get("Body",None)
 
+    # url format
+    url = urlparse(arg)
+
+    # if not valid url
+    if url.scheme == '':
+        resp.message("Invalid url format!")
+        return str(resp)
+
     #there was no message body so send something silly
     if not arg:
     	resp.message("The Robots are coming! Head for the hills!")
     	return str(resp)
     extern_resp = requests.get(arg)
+    
     #we couldnt get the resource
     if extern_resp.status_code != 200:
         resp.message("Couldnt get resource soz")
