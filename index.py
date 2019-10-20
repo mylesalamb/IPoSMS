b230ea8a02ebb8c8c792ac630ddfab2dd87dd83a
Miles Grant <mlsgrnt@gmail.com>
2019-10-20 13:03:22 +0100

    Strip irrelevant HTML tags

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ import json
 import zlib
 import requests
 import base64
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -17,15 +18,22 @@ def sms_reply():
 
     r = requests.get(body)
 
+    # Strip out script and style tags
+    soup = BeautifulSoup(r.text, 'lxml')
+    [s.extract() for s in soup('script')]
+    [s.extract() for s in soup('link')]
+    [s.extract() for s in soup('meta')]
+    [s.extract() for s in soup('style')]
+
     # Compress the result
-    processed = bytearray(r.text, 'utf-8')
+    processed = bytearray(str(soup), 'utf-8')
     processed = zlib.compress(processed)
     processed = base64.b64encode(processed)
 
     n = 1000 # Chunk the string into groups of 1000 characters to stay well below the 1600 character limit
     parts = [processed[i:i+n] for i in range(0, len(processed), n)]
     for chunk in parts:
-        resp.message(str(chunk)[2:-1])
+        resp.message(str(chunk)[2:-1]) # Python appends some garbage to the result
     resp.message('=====') # This is a good way to declare the end
     return str(resp)
 
